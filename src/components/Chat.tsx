59366e4b01ddef328d67795432a9dcf394034dfe
Patticatti <xpatticattix@gmail.com>
2024-03-09 17:25:25 -0800

    Finished suggestions

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Grid } from '@chakra-ui/react';
 import ChatOption from './ChatOption';
 
 function Chat() {
-  return <ChatOption image="/eggIcon.svg" desc="Learn your rights" title="Fertility Rights" />;
+  return (
+    <Grid
+      templateColumns="repeat(2, 1fr)" // Adjust the number of columns as needed
+      gap={4} // Adjust the gap between grid items as needed
+      width="fit-content"
+    >
+      <ChatOption image="/egg-svgrepo-com 1 (2).svg" desc="Learn your rights" title="Fertility Rights" />
+      <ChatOption image="/eggIcon.svg" desc="Learn your rights" title="Fertility Rights" />
+      <ChatOption image="/femalesIcon.svg" desc="Learn your rights" title="Fertility Rights" />
+      <ChatOption image="/medicalIcon.svg" desc="Learn your rights" title="Fertility Rights" />
+    </Grid>
+  );
 }
 
 export default Chat;
