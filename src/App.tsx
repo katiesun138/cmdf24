775c36b193ee82c64304167d741e2c7b0c1692df
Chloé Benko-Prieur <chloebenkoprieur@gmail.com>
2024-03-09 23:15:38 -0800

    added chatmenu

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import './App.css';
 import React from 'react';
-import Chat from './components/Chat';
+import ChatMenu from './components/ChatMenu';
+import ChatBackend from './components/ChatBackend';
 import Location from './components/Location';
 import Learn from './components/Learn';
 import Prompts from './components/Prompts';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
 import { Switch } from '@chakra-ui/react';
 
 
 function App() {
   const [messages, setMessages] = React.useState<string[]>([]);
+  const [selectedOption, setSelectedOption] = React.useState<'livechat' | 'prompts' | null>(null);
+
+  const handleOptionClick = (option: 'livechat' | 'prompts') => {
+    setSelectedOption(option);
+  };
 
   const handleSendMessage = async (message: string) => {
+    
     setMessages([...messages, message]);
 
     console.log("handleSendMessage");
@@ -29,9 +37,8 @@ function App() {
     try {
       const response = await fetch('http://localhost:8080/bigsister', userInputRequest)
       const data = await response.json()
-      console.log(data)
       return data;
-      
+
     } catch(err) {
       console.log(err);
     }
@@ -42,8 +49,9 @@ function App() {
         <Routes>
           <Route path="/location" element={<Location/>} />
           <Route path="/learn" element={<Learn/>} />
-          <Route path="/" element={<Chat onSendMessage={handleSendMessage}/>} />
-          <Route path="/prompts" element={<Prompts onClick={handleSendMessage}/>} />
+          <Route path="/" element={<ChatMenu onClick={handleOptionClick}/>} />
+          {selectedOption === 'livechat' && <Route path="/livechat" element={<ChatBackend onSendMessage={handleSendMessage} />} />}
+          {selectedOption === 'prompts' && <Route path="/prompts" element={<Prompts onClick={handleSendMessage} />} />}
         </Routes>
     </BrowserRouter>
   );
