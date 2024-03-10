3041a02c6fd4cb69401374471d6af91c7f775c86
Patticatti <xpatticattix@gmail.com>
2024-03-09 20:00:13 -0800

    Syntax error

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -5,13 +5,14 @@ import Map from './Map';
 interface MyLocation {
   long: number;
   lat: number;
+}
 
 const Location: React.FC = () => {
   const [myLoc, setMyLoc] = useState<MyLocation>({ long: 0, lat: 0 });
 
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: 'AIzaSyCQCRo-wYWlvKLNYH8xd_FbmnqGlHwl9Lg',
-    libraries: ["places"],
+    libraries: ['places'],
   });
 
   useEffect(() => {
@@ -19,7 +20,7 @@ const Location: React.FC = () => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(success, error);
     } else {
-      console.log("Geolocation not supported");
+      console.log('Geolocation not supported');
     }
 
     function success(position: any) {
@@ -30,13 +31,13 @@ const Location: React.FC = () => {
     }
 
     function error() {
-      console.log("Unable to retrieve your location");
+      console.log('Unable to retrieve your location');
     }
   }, []); // Empty dependency array to run the effect only once on mount
 
   if (!isLoaded) return <div>Loading...</div>;
 
-  return <Map long={myLoc.long} lat = {myLoc.lat}/>;
+  return <Map long={myLoc.long} lat={myLoc.lat} />;
 };
 
 export default Location;
