e607fdedbbddc487307c38e79736e4aa3515f6b7
Patticatti <xpatticattix@gmail.com>
2024-03-10 03:21:10 -0700

    Fixed the tinymce shiet

--- frontend/src/components/TinyMCE.tsx
+++ frontend/src/components/TinyMCE.tsx
@@ -1,30 +1,65 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import { Editor } from '@tinymce/tinymce-react';
+import { Button, Input, Flex } from '@chakra-ui/react';
 
-const MyEditor = () => {
-  const handleEditorChange = (content:any, editor:any) => {
-    console.log('Content was updated:', content);
+interface Props {
+  onClick: (desc: string, title: string) => void; // Define the type of the prop
+  backToForum: () => void;
+}
+let contentOutput: string;
+let titleOutput: string;
+
+const MyEditor: React.FC<Props> = ({ onClick, backToForum }) => {
+  const handleEditorChange = (content: string, editor: any) => {
+    contentOutput = content;
   };
-
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    titleOutput = event.target.value;
+  };
+  const handleClick = () => {
+    onClick(contentOutput, titleOutput);
+    backToForum();
+  };
   return (
-    <Editor
-      apiKey="otbou93omboff3xmdezdhk0ty9nxv45org5g34jkuiyq3q3z" // You can get an API key by signing up on the TinyMCE website
-      initialValue="<p>This is the initial content of the editor</p>"
-      init={{
-        height: 500,
-        menubar: false,
-        plugins: [
-          'advlist autolink lists link image charmap print preview anchor',
-          'searchreplace visualblocks code fullscreen',
-          'insertdatetime media table paste code help wordcount',
-        ],
-        toolbar:
-          'undo redo | formatselect | bold italic backcolor | \
+    <>
+      <Input
+        variant="outline"
+        borderRadius="10px"
+        bg="white"
+        borderColor="gray.200" // Set the border color to gray
+        borderWidth="1px" // Set the border width to 1px
+        color="gray.800"
+        placeholder="Title"
+        _placeholder={{ color: 'gray.400', fontSize: 'sm' }}
+        onChange={handleInputChange}
+      />
+      <Editor
+        apiKey="otbou93omboff3xmdezdhk0ty9nxv45org5g34jkuiyq3q3z"
+        initialValue=""
+        init={{
+          height: 500,
+          menubar: false,
+          plugins: [
+            'advlist autolink lists link image charmap print preview anchor',
+            'searchreplace visualblocks code fullscreen',
+            'insertdatetime media table paste code help wordcount',
+          ],
+          toolbar:
+            'undo redo | formatselect | bold italic backcolor | \
           alignleft aligncenter alignright alignjustify | \
           bullist numlist outdent indent | removeformat | help',
-      }}
-      onEditorChange={handleEditorChange}
-    />
+        }}
+        onEditorChange={handleEditorChange}
+      />
+      <Flex gap="0.5rem" justifyContent="flex-end">
+        <Button variant="white" paddingInline="32px">
+          Save as draft
+        </Button>
+        <Button variant="hotpink" paddingInline="32px" onClick={handleClick}>
+          Post
+        </Button>
+      </Flex>
+    </>
   );
 };
 
