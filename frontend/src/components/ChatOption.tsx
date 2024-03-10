438468e12eefa68b48e695c4272b2ea9c0ed134b
Patticatti <xpatticattix@gmail.com>
2024-03-10 07:21:15 -0700

    Styled and connected the chat pages it's done except prompts

--- frontend/src/components/ChatOption.tsx
+++ frontend/src/components/ChatOption.tsx
@@ -1,31 +1,42 @@
-import { Flex, Text, Stack, Image } from '@chakra-ui/react';
-import React from 'react';
+import { Flex, Text, Stack, Icon } from '@chakra-ui/react';
+import { MdSupportAgent } from 'react-icons/md';
+import { FaWandMagicSparkles } from 'react-icons/fa6';
 
 interface ChatOptionProps {
   desc: string;
   title: string;
-  image: string;
+  hasIcon: boolean;
 }
 
-function ChatOption({ desc, title, image }: ChatOptionProps) {
+function ChatOption({ desc, title, hasIcon }: ChatOptionProps) {
   return (
     <Flex
       padding="1rem"
       justifyContent="center"
       gap="0.5rem"
+      bg="white"
       alignItems="center"
       borderRadius="24px"
-      height="62px"
+      height="78px"
       border="1px solid rgba(0, 0, 0, 0.20)"
     >
       <Flex justifyContent="center" alignItems="center" borderRadius="4px" width="36px" height="36px" border="1px solid rgba(0, 0, 0, 0.20)">
-        <Image src={image} alt="Description of the image" style={{ maxWidth: '100%', maxHeight: '100%' }} />
+        {hasIcon && (
+          <Flex justifyContent="center" alignItems="center" borderRadius="4px" width="36px" height="36px" border="1px solid rgba(0, 0, 0, 0.20)">
+            <MdSupportAgent color="hotpink" size="1.6rem" />
+          </Flex>
+        )}
+        {!hasIcon && (
+          <Flex justifyContent="center" alignItems="center" borderRadius="4px" width="36px" height="36px" border="1px solid rgba(0, 0, 0, 0.20)">
+            <FaWandMagicSparkles color="hotpink" size="1.6rem" />
+          </Flex>
+        )}
       </Flex>
-      <Stack gap="0">
-        <Text fontWeight="800" fontSize="2xs">
+      <Stack gap="0" textAlign="left">
+        <Text fontWeight="800" fontSize="md">
           {title}
         </Text>
-        <Text color="black" opacity="0.6" fontSize="2xs">
+        <Text color="black" opacity="0.6" fontSize="sm">
           {desc}
         </Text>
       </Stack>
