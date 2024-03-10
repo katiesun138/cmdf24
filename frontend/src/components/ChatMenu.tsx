2b11a6efd6bd0fef4b94faea699a63e3944db996
Chloé Benko-Prieur <chloebenkoprieur@gmail.com>
2024-03-10 06:46:38 -0700

    started adding styling for menu page

--- frontend/src/components/ChatMenu.tsx
+++ frontend/src/components/ChatMenu.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { LuKeyboard, LuSend } from 'react-icons/lu';
 import { Link } from "react-router-dom";
+import { Flex, Box, Input, Button, InputGroup, InputLeftElement, InputRightElement, Grid, Stack, Heading, Text } from '@chakra-ui/react';
 
 
 interface ChatMenuProps {
@@ -12,12 +14,39 @@ const ChatMenu: React.FC<ChatMenuProps> = ({ onClick }) => {
   };
 
   return (
-    <>
-      <div>THIS IS THE ChatMenu</div>
-      <Link to="/livechat"><button onClick={() => handleOptionClick('livechat')}>Live Chat</button></Link>
-      <div></div>
-      <Link to="/prompts"><button onClick={() => handleOptionClick('prompts')}>Prompts</button></Link>     
-    </>
+    <Stack width="100%" height="100%" justifyContent="center" padding="0.5rem">
+        <Heading fontSize="md" fontWeight="600">
+          Chat with BigSister
+        </Heading>
+
+        <Stack spacing={2}>
+            <Box
+              textAlign={'center'}
+              bg={'hotpink'}
+              color="white"
+              p={2}
+              borderRadius="md"
+            >
+              <Text fontSize="1.1rem" color='white'>{"Hi, I'm BigSister!"}</Text>
+              <Text color='white'>I'm here to give you information about abortion and provide support if needed. What chat option do you prefer?"</Text>
+            </Box>
+        </Stack>
+        <Flex justifyContent="center" mt={4}>
+        <Link to="/livechat"><Button colorScheme="hotpink" mx={2} minWidth="200px" onClick={() => handleOptionClick('livechat')}>
+          <Flex direction="column" alignItems="center" whiteSpace="normal">
+            <Text fontSize="1.5rem">Live Chat</Text>
+            <Text fontSize="0.8rem">Type your own questions and receive answers right away!</Text>
+          </Flex>
+        </Button></Link>
+        <Link to="/prompts"><Button colorScheme="hotpink" mx={2} minWidth="200px" onClick={() => handleOptionClick('prompts')}>
+          <Flex direction="column" alignItems="center" whiteSpace="normal">
+            <Text fontSize="1.5rem">Suggested Prompts</Text>
+            <Text fontSize="0.8rem">If you don't feel like typing, this option allows you to select prompts to learn more about abortion.</Text>
+          </Flex>
+        </Button></Link>
+        </Flex>
+        
+      </Stack>
   );
 };
 
